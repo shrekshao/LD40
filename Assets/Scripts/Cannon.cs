c4c495d6a08853a65d17becac40bb441ae2ad6b5
shrekshao <shrekshao@gmail.com>
2017-12-01 22:20:23 -0800

    some basic setup of tetris tank idea

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+
+    public Rigidbody m_rigidBodyTank;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fire());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Fire()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            m_rigidBodyTank.AddForceAtPosition(-transform.forward, transform.position, ForceMode.Impulse);
+        }
+       
+    }
+}
